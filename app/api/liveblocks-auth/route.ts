2107755f99d5f496ef086a6be5270c0c45af2937
adenkesuma <adenfdfd10@gmail.com>
2024-02-10 15:59:30 +0700

    add room authentication

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -14,24 +14,12 @@ export async function POST(request: Request) {
   const authorization = await auth()
   const user = await currentUser()
 
-  console.log("AUTH_INFO", {
-    authorization,
-    user
-  })
-
   if (!authorization || !user) {
     return new Response("Unauthorized", { status: 403 })
   }
 
   const { room } = await request.json()
   const board = await convex.query(api.board.get, { id: room })
-
-  console.log("AUTH_INFO_2", {
-    room,
-    board,
-    boardOrgId: board?.orgId,
-    userOrgId: authorization.orgId
-  })
 
   if (board?.orgId !== authorization.orgId) {
     return new Response("Unauthorized", { status: 403 })
@@ -41,10 +29,6 @@ export async function POST(request: Request) {
     name: user.firstName || "Teammate",
     picture: user.imageUrl
   }
-
-  console.log("USER_INFO", {
-    userInfo
-  })
 
   const session = liveblocks.prepareSession(
     user.id,
@@ -56,11 +40,6 @@ export async function POST(request: Request) {
   }
 
   const { status, body } = await session.authorize()
-
-  console.log("ALLOWED", {
-    status, 
-    body
-  })
 
   return new Response(body, { status })
 }
